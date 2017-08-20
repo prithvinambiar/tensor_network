5fc3f8283354ed9596b05914d3d28857678d247b
Prithvi Nambiar <prithvinambiar@gmail.com>
2017-08-20 16:09:39 +0530

    Added support for tensorboard graph visualisation - The data will be located in  tempfile.gettempdir() + "/tensorflow" folder

--- tensor_network/neural_net/back_propagation.py
+++ tensor_network/neural_net/back_propagation.py
@@ -1,12 +1,20 @@
+import logging
+import tempfile
+
 import tensorflow as tf
 
 
 def initialize_weight(input_count, output_count):
-    return tf.Variable(tf.truncated_normal([input_count, output_count]))
+    return tf.Variable(tf.truncated_normal([input_count, output_count]), name='W')
 
 
 def initialize_bias(output):
-    return tf.Variable(tf.truncated_normal([output]))
+    return tf.Variable(tf.truncated_normal([output]), name='B')
+
+
+def initialize_layer(layer_info, name='layer'):
+    with tf.name_scope(name):
+        return initialize_weight(layer_info[0], layer_info[1]), initialize_bias(layer_info[1])
 
 
 def initialize_network(input_count, list_of_neurons):
@@ -14,9 +22,12 @@ def initialize_network(input_count, list_of_neurons):
     # list2 = list_of_neurons + [output_count]
     list2 = list_of_neurons
 
-    def func(d): return initialize_weight(d[0], d[1]), initialize_bias(d[1])
-
-    return map(func, list(zip(list1, list2)))
+    layers = []
+    i = 1
+    for layer_info in list(zip(list1, list2)):
+        layers.append(initialize_layer(layer_info, name='layer' + str(i)))
+        i += 1
+    return layers
 
 
 def tensor_network(first_layer_input, list_of_neurons):
@@ -34,27 +45,42 @@ def evaluate_network(network, input_data):
 
 class BackPropagation:
     def __init__(self, number_of_features, number_of_output, neurons_list):
-        self.__session = tf.Session()
-        self.__x = tf.placeholder(tf.float32, [None, number_of_features])
-        self.__y = tf.placeholder(tf.float32, [None, number_of_output])
-        self.__network = list(tensor_network(number_of_features, neurons_list))
-        self.model = evaluate_network(self.__network, self.__x)
+        self.session = tf.Session()
+        self.x = tf.placeholder(tf.float32, [None, number_of_features])
+        self.y = tf.placeholder(tf.float32, [None, number_of_output])
+        self.network = list(tensor_network(number_of_features, neurons_list))
+        self.model = evaluate_network(self.network, self.x)
+        with tf.name_scope("cost"):
+            self.cost_function = tf.reduce_mean(
+                -tf.reduce_sum((self.y * tf.log(self.model) + ((1 - self.y) * tf.log(1 - self.model))), axis=[1]))
 
     def __del__(self):
-        self.__session.close()
+        self.session.close()
+
+    def setup_tensor_graph(self):
+        temp_dir = tempfile.gettempdir() + "/tensorflow"
+        logging.info("Logging TensorFlow data to %s " % temp_dir)
+        writer = tf.summary.FileWriter(temp_dir)
+        writer.add_graph(self.session.graph)
 
     def train(self, train_input_data, train_output_data, iterations=10000):
-        self.__session.run(tf.global_variables_initializer())
-        cross_entropy = tf.reduce_mean(
-            -tf.reduce_sum((self.__y * tf.log(self.model) + ((1 - self.__y) * tf.log(1 - self.model))), axis=[1]))
-        train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
+        self.setup_tensor_graph()
+        self.session.run(tf.global_variables_initializer())
+        with tf.name_scope("train"):
+            train_step = tf.train.GradientDescentOptimizer(0.05).minimize(self.cost_function)
         for i in range(iterations):
             if i % 1000 == 0:
-                print(self.__session.run(cross_entropy,
-                                         feed_dict={self.__x: train_input_data, self.__y: train_output_data}))
-                for j in self.__network:
-                    print(self.__session.run(j))
-            self.__session.run(train_step, feed_dict={self.__x: train_input_data, self.__y: train_output_data})
+                print(self.session.run(self.cost_function,
+                                       feed_dict={self.x: train_input_data, self.y: train_output_data}))
+                for j in self.network:
+                    print(self.session.run(j))
+            self.session.run(train_step, feed_dict={self.x: train_input_data, self.y: train_output_data})
 
     def predict(self, test_input_data):
-        return self.__session.run(self.model, feed_dict={self.__x: test_input_data})
+        return self.session.run(self.model, feed_dict={self.x: test_input_data})
+
+    def validation(self, validation_input_data, validation_output_data):
+        with tf.name_scope("validation"):
+            predictions = self.predict(validation_input_data)
+            return self.session.run(self.cost_function,
+                                    feed_dict={self.y: validation_output_data, self.model: predictions})
--- tests/neural_net/test_back_propagation.py
+++ tests/neural_net/test_back_propagation.py
@@ -11,3 +11,13 @@ def test_train_neural_net_for_or_gate():
     assert bp.predict([[1, 0]]) > [[0.75]]
     assert bp.predict([[0, 1]]) > [[0.75]]
     assert bp.predict([[1, 1]]) > [[0.75]]
+
+
+def test_validation_log_loss_should_be_below_10():
+    input_data = [[0, 0], [0, 1], [1, 0], [1, 1]]
+    output_data = [[0], [1], [1], [1]]
+
+    bp = back_propagation.BackPropagation(2, 1, [1])
+    bp.train(input_data, output_data)
+    print(bp.validation(input_data, output_data))
+    assert bp.validation(input_data, output_data) < 0.10
